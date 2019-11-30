cf6c5e7982fa9a69d8af372c02a290b24468bdaa
Rado Jedro <radoslaw.jedro@gmail.com>
2019-11-30 13:56:27 +0000

    Add filter selectors

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -21,19 +21,21 @@ export const getFilteredTrips = ({trips, filters}) => {
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
 
   // TODO - filter trips by tripId
+  const filtered = trips.filter(trip => tripId ===trip.id);
+
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  //const filtered = trips;
 
   // TODO - filter trips by countryCode
 
+  const filtered = trips.filter(trip => countryCode ===trip.country.code);
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
 };

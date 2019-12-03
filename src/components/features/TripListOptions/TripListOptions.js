72a0b92268d271e85e5b0d3309ecc2c2a414aa43
Rado Jedro <radoslaw.jedro@gmail.com>
2019-12-03 00:32:07 +0000

    Add duration filter

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,18 +9,18 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
-      this.props.addTag(tag,checked);
+      this.props.addTag(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
-      this.props.removeTag(tag,checked);
+      this.props.removeTag();
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
-    this.props.changeDuration(type, value);
+    this.props.changeDuration({[type]: value});
   }
 
   handleSearch(phrase){

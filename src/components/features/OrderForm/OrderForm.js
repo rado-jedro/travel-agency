67ef120b5f5ba9fc9f6a3df8bc9b7014e89e8a84
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-09 22:08:22 +0000

    Add props

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
 
 
-const OrderForm = () => (
+const OrderForm = props => (
   <Row>
     <Col xs={12}>
-      <OrderSummary />
+      <OrderSummary tripCost={props.tripCost} options={props.options}/>
     </Col>
   </Row>
 );
 
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderForm;

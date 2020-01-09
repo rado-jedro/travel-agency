ccc63a1e9de239034b3a60a6a732649eb28768e2
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-09 22:08:50 +0000

    Add props

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from '../OrderSummary/OrderSummary.scss';
 
-const OrderSummary = () => (
+
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+
+const OrderSummary = props => (
   <h2 className={styles.component}>
-    Total: <strong>$12,345</strong>
+    Total: <strong>${calculateTotal(formatPrice(props.tripCost), props.options)}</strong>
   </h2>
 );
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
 
 export default OrderSummary;
 

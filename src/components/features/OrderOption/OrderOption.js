bb21d0bf65670b525301cba3f8a1582d7c709877
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-12 23:01:06 +0000

    Add OrderOprions types

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import styles from  '../OrderOption/OrderOption.scss';
 
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
 
-const OrderOption = () => (
-  <div className={styles.component}>
-    <h3 className={styles.title}>
-      Car Rental
-    </h3>
-  </div>
-);
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+  if(!OptionComponent){
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
 
 export default OrderOption;

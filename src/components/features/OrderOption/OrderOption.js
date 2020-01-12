78bde92169f36616928f55e819df56ebb60cf527
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-12 11:55:35 +0000

    Create OrderOption.js

---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from  '../OrderOption/OrderOption.scss';
+
+
+const OrderOption = () => (
+  <div className={styles.component}>
+    <h3 className={styles.title}>
+      Car Rental
+    </h3>
+  </div>
+);
+
+export default OrderOption;

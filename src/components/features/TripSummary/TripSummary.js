c5dda480620e5edaa09f6631ecb579f703c855ab
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-26 13:55:21 +0000

    Add isRequired to props

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -25,11 +25,11 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
 );
 
 TripSummary.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   image: PropTypes.string,
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
   intro: PropTypes.string,
-  cost: PropTypes.string,
+  cost: PropTypes.string.isRequired,
   days: PropTypes.number,
   tags: PropTypes.array,
 };

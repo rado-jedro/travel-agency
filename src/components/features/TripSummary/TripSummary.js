bfdd0421f56ca99bf44c80ad28b5d4deb878d287
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-05 22:41:40 +0000

    Add isRequired to rops image and days

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -28,11 +28,11 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
 
 TripSummary.propTypes = {
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  image: PropTypes.string,
+  image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   intro: PropTypes.string,
   cost: PropTypes.string.isRequired,
-  days: PropTypes.number,
+  days: PropTypes.number.isRequired,
   tags: PropTypes.array,
 };
 

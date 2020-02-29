1a9eca132494e43a483813894def1f70fa10d031
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-29 18:57:26 +0000

    Add test4 - correct seconds dispaly

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -6,11 +6,34 @@ class HappyHourAd extends React.Component {
   static propTypes = {
     title: PropTypes.node,
   };
+
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(
+      Date.UTC(
+        currentTime.getUTCFullYear(),
+        currentTime.getUTCMonth(),
+        currentTime.getUTCDate(),
+        12,
+        0,
+        0,
+        0
+      )
+    );
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
+  }
+
+
   render() {
     return <div className={styles.component}>
       <div className={styles.component}>
         <h3 className={styles.title}>{this.props.title}</h3>
-        <div className={styles.countdown}></div>
+        <div className={styles.countdown}>{this.getCountdownTime()}</div>
       </div>
     </div>;
   }

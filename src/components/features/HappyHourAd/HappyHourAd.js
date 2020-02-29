5faae046908e481b0d8fa3b5e5c2a5f734231794
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-29 18:18:56 +0000

    Add test1 - render component

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '.HappyHourAd.scss';
+
+class HappyHourAd extends React.Component {
+  render() {
+    return <div className={styles.component}></div>;
+  }
+}
+
+export default HappyHourAd;
